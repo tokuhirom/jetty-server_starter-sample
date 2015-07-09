36521ec335b73f3c00bd277099179294a6f0bae4
Tokuhiro Matsuno <tokuhirom@gmail.com>
2015-07-09 23:32:12 +0900

    Do not send 'Server' header

--- src/main/java/me/geso/example/Httpd.java
+++ src/main/java/me/geso/example/Httpd.java
@@ -11,17 +11,24 @@ import java.lang.management.ManagementFactory;
 
 import org.eclipse.jetty.jmx.MBeanContainer;
 import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.Slf4jRequestLog;
 import org.eclipse.jetty.server.handler.HandlerCollection;
 import org.eclipse.jetty.server.handler.RequestLogHandler;
 import org.eclipse.jetty.server.handler.StatisticsHandler;
-import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 public class Httpd {
 	public static void main(String[] args) throws Exception {
+		HttpConfiguration httpConfig = new HttpConfiguration();
+		// do not send "Server" header
+		httpConfig.setSendServerVersion( false );
+		HttpConnectionFactory httpFactory = new HttpConnectionFactory( httpConfig );
+
 		int port = Integer.valueOf(System.getProperty("jetty.port", "18080"));
 		Server server = new Server();
 
